f7052f9dc996da4cc984fbeb8839b4b22f7f99f8
David Dunn <david.dunn@displaylink.com>
2019-09-30 09:00:08 +0100

    Add tests for all consonants

--- c++/src/test.cpp
+++ c++/src/test.cpp
@@ -15,13 +15,6 @@ TEST_F(soundex_encoding, zero_pads_word)
   ASSERT_THAT(encoded, Eq("A000"));
 }
 
-TEST_F(soundex_encoding, encodes_consonants)
-{
-  ASSERT_THAT(soundex.encode("Ab"), Eq("A100"));
-  ASSERT_THAT(soundex.encode("Ac"), Eq("A200"));
-  ASSERT_THAT(soundex.encode("Ad"), Eq("A300"));
-}
-
 TEST_F(soundex_encoding, skips_vowels)
 {
   ASSERT_THAT(soundex.encode("Ba"), Eq("B000"));
@@ -31,3 +24,40 @@ TEST_F(soundex_encoding, skips_duplicates)
 {
   ASSERT_THAT(soundex.encode("Abc"), Eq("A120"));
 }
+
+class ConsonantTest:
+  public testing::TestWithParam<std::tuple<const char*, const char*>>
+{
+public:
+  Soundex soundex;
+  std::string in;
+  std::string out;
+
+  void SetUp()
+  {
+    std::tie(in, out) = GetParam();
+  }
+};
+
+TEST_P(ConsonantTest, fred)
+{
+  ASSERT_THAT(soundex.encode(in), Eq(out));
+}
+
+INSTANTIATE_TEST_SUITE_P(BLike, ConsonantTest,
+  Combine(Values("Ab", "Af", "Ap", "Av"), Values("A100")));
+
+INSTANTIATE_TEST_SUITE_P(CLike, ConsonantTest,
+  Combine(Values("Ac", "Ag", "Aj", "Ak", "Aq", "As", "Ax", "Az"), Values("A200")));
+
+INSTANTIATE_TEST_SUITE_P(DLike, ConsonantTest,
+  Combine(Values("Ad", "At"), Values("A300")));
+
+INSTANTIATE_TEST_SUITE_P(LLike, ConsonantTest,
+  Values(std::make_tuple("Al", "A400")));
+
+INSTANTIATE_TEST_SUITE_P(MLike, ConsonantTest,
+  Combine(Values("Am", "An"), Values("A500")));
+
+INSTANTIATE_TEST_SUITE_P(RLike, ConsonantTest,
+  Values(std::make_tuple("Ar", "A600")));
